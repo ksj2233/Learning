1854f24bca80a515e1f5ff44b6202a3c08e7676e
sungjin Kim <ksjksb@naver.com>
2022-10-24 15:49:45 +0900

    10월 24일 수업2

--- chapter08/sec03/ZonedDateTimeDemo2.java
+++ chapter08/sec03/ZonedDateTimeDemo2.java
@@ -1,18 +1,24 @@
 package sec03;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.ZonedDateTime;
 
 public class ZonedDateTimeDemo2 {
 
 	public static void main(String[] args) {
 		// 한국 출발 현지 시간 2022년 12월 9일 13시 20분
-		ZonedDateTime seoul = ZonedDateTime.of(LocalDateTime.of(2022, 12, 9, 13, 20), null);
+		ZonedDateTime seoul = ZonedDateTime.of(LocalDateTime.of(2022, 12, 9, 13, 20), ZoneId.of("Asia/Seoul"));
 		System.out.println(seoul);
 
 		// 파리 도착 시간 2022년 12월 9일 17시 15분
+		ZonedDateTime paris = ZonedDateTime.of(LocalDateTime.of(2022, 12, 9, 17, 15), ZoneId.of("Europe/Paris"));
+		System.out.println(paris);
 
 		// 비행에 걸린 시간은?
+		Duration diff = Duration.between(seoul, paris);
+		System.out.println(diff);
 
 	}
 
